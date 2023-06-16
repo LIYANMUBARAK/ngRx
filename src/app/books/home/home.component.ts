edeb70c0c61eaddde758fac191ba60fd1c48f737
LIYANMUBARAK <liyanmubarakc@gmail.com>
2023-06-16 09:26:32 +0530

    selector reducer added

--- src/app/books/home/home.component.ts
+++ src/app/books/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { selectBooks } from '../store/books.selector';
+import { select,Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  constructor(private store:Store){}
+  books$ = this.store.pipe(select(selectBooks))
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/books/store/books.reducer.ts
+++ src/app/books/store/books.reducer.ts
@@ -1,2 +1,12 @@
-export class BooksReducer {
-}
+
+import { createReducer } from "@ngrx/store"
+import { Book } from "./book"
+
+export const initialState :ReadonlyArray<Book>=[{
+    "id":1,
+    "title":"testing name 1",
+    "cost": 2499}] 
+
+export const bookReducer = createReducer(
+    initialState
+)
